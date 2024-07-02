82987fe86c076d69fddeb7f21cea87ce1a1b4202
Dharun kumar <dharun.official.01@gmail.com>
2024-07-02 12:01:57 +0530

    Update c3-main-attempt.cpp

--- Scan-Matching-Localisation/c3-main-attempt.cpp
+++ Scan-Matching-Localisation/c3-main-attempt.cpp
@@ -8,9 +8,7 @@
 #include <carla/sensor/data/LidarMeasurement.h>
 #include <thread>
 
-#include <carla/client/Vehicle.h>
-
-// pcl code
+// PCL includes
 #include <pcl/io/pcd_io.h>
 #include <pcl/visualization/pcl_visualizer.h>
 #include <pcl/filters/voxel_grid.h>
@@ -18,6 +16,7 @@
 #include <pcl/registration/ndt.h>
 #include <pcl/console/time.h>   // TicToc
 #include "helper.h"
+
 #include <sstream>
 #include <chrono> 
 #include <ctime> 
@@ -31,6 +30,11 @@ using namespace std::string_literals;
 
 using namespace std;
 
+// PCL point cloud type
+typedef pcl::PointXYZ PointT;
+typedef pcl::PointCloud<PointT> PointCloudT;
+
+// Global variables
 PointCloudT pclCloud;
 cc::Vehicle::Control control;
 std::chrono::time_point<std::chrono::system_clock> currentTime;
@@ -38,6 +42,7 @@ vector<ControlState> cs;
 
 bool refresh_view = false;
 
+// Keyboard event callback
 void keyboardEventOccurred(const pcl::visualization::KeyboardEvent &event, void* viewer) {
     if (event.getKeySym() == "Right" && event.keyDown()) {
         cs.push_back(ControlState(0, -0.02, 0));
@@ -54,27 +59,29 @@ void keyboardEventOccurred(const pcl::visualization::KeyboardEvent &event, void*
     }
 }
 
+// Vehicle control logic
 void Accuate(ControlState response, cc::Vehicle::Control& state) {
     if (response.t > 0) {
         if (!state.reverse) {
-            state.throttle = min(state.throttle + response.t, 1.0f);
+            state.throttle = std::min(state.throttle + response.t, 1.0f);
         } else {
             state.reverse = false;
-            state.throttle = min(response.t, 1.0f);
+            state.throttle = std::min(response.t, 1.0f);
         }
     } else if (response.t < 0) {
         response.t = -response.t;
         if (state.reverse) {
-            state.throttle = min(state.throttle + response.t, 1.0f);
+            state.throttle = std::min(state.throttle + response.t, 1.0f);
         } else {
             state.reverse = true;
-            state.throttle = min(response.t, 1.0f);
+            state.throttle = std::min(response.t, 1.0f);
         }
     }
-    state.steer = min(max(state.steer + response.s, -1.0f), 1.0f);
+    state.steer = std::min(std::max(state.steer + response.s, -1.0f), 1.0f);
     state.brake = response.b;
 }
 
+// Draw the vehicle in the viewer
 void drawCar(Pose pose, int num, Color color, double alpha, pcl::visualization::PCLVisualizer::Ptr& viewer) {
     BoxQ box;
     box.bboxTransform = Eigen::Vector3f(pose.position.x, pose.position.y, 0);
@@ -85,8 +92,8 @@ void drawCar(Pose pose, int num, Color color, double alpha, pcl::visualization::
     renderBox(viewer, box, num, color, alpha);
 }
 
+// Perform ICP (Iterative Closest Point) pose estimation
 Pose performICP(PointCloudT::Ptr target, PointCloudT::Ptr source) {
-    // Create the ICP object
     pcl::IterativeClosestPoint<PointT, PointT> icp;
     icp.setMaximumIterations(50);
     icp.setInputSource(source);
@@ -101,8 +108,8 @@ Pose performICP(PointCloudT::Ptr target, PointCloudT::Ptr source) {
     return pose;
 }
 
+// Perform NDT (Normal Distributions Transform) pose estimation
 Pose performNDT(PointCloudT::Ptr target, PointCloudT::Ptr source) {
-    // Create the NDT object
     pcl::NormalDistributionsTransform<PointT, PointT> ndt;
     ndt.setMaximumIterations(50);
     ndt.setResolution(1.0);
@@ -130,9 +137,8 @@ int main() {
     auto transform = map->GetRecommendedSpawnPoints()[1];
     auto ego_actor = world.SpawnActor((*vehicles)[12], transform);
 
-    // Create lidar
+    // Create lidar sensor
     auto lidar_bp = *(blueprint_library->Find("sensor.lidar.ray_cast"));
-    // CANDO: Can modify lidar values to get different scan resolutions
     lidar_bp.SetAttribute("upper_fov", "15");
     lidar_bp.SetAttribute("lower_fov", "-25");
     lidar_bp.SetAttribute("channels", "32");
@@ -145,24 +151,28 @@ int main() {
     auto lidar_actor = world.SpawnActor(lidar_bp, lidar_transform, ego_actor.get());
     auto lidar = boost::static_pointer_cast<cc::Sensor>(lidar_actor);
     bool new_scan = true;
-    std::chrono::time_point<std::chrono::system_clock> lastScanTime, startTime;
+    std::chrono::time_point<std::chrono::system_clock> lastScanTime;
 
+    // PCL viewer setup
     pcl::visualization::PCLVisualizer::Ptr viewer (new pcl::visualization::PCLVisualizer ("3D Viewer"));
     viewer->setBackgroundColor(0, 0, 0);
     viewer->registerKeyboardCallback(keyboardEventOccurred, (void*)&viewer);
 
+    // Get vehicle and initial pose
     auto vehicle = boost::static_pointer_cast<cc::Vehicle>(ego_actor);
     Pose pose(Point(0, 0, 0), Rotate(0, 0, 0));
 
-    // Load map
+    // Load map point cloud
     PointCloudT::Ptr mapCloud(new PointCloudT);
     pcl::io::loadPCDFile("map.pcd", *mapCloud);
     cout << "Loaded " << mapCloud->points.size() << " data points from map.pcd" << endl;
     renderPointCloud(viewer, mapCloud, "map", Color(0, 0, 1));
 
+    // Define filtered and scan point clouds
     typename pcl::PointCloud<PointT>::Ptr cloudFiltered(new pcl::PointCloud<PointT>);
     typename pcl::PointCloud<PointT>::Ptr scanCloud(new pcl::PointCloud<PointT>);
 
+    // Lidar listener
     lidar->Listen([&new_scan, &lastScanTime, &scanCloud](auto data) {
         if (new_scan) {
             auto scan = boost::static_pointer_cast<csd::LidarMeasurement>(data);
@@ -171,7 +181,7 @@ int main() {
                     pclCloud.points.push_back(PointT(detection.x, detection.y, detection.z));
                 }
             }
-            if (pclCloud.points.size() > 5000) { // CANDO: Can modify this value to get different scan resolutions
+            if (pclCloud.points.size() > 5000) {
                 lastScanTime = std::chrono::system_clock::now();
                 *scanCloud = pclCloud;
                 new_scan = false;
@@ -179,32 +189,37 @@ int main() {
         }
     });
 
+    // Initial reference pose
     Pose poseRef(Point(vehicle->GetTransform().location.x, vehicle->GetTransform().location.y, vehicle->GetTransform().location.z),
-                 Rotate(vehicle->GetTransform().rotation.yaw * pi / 180, vehicle->GetTransform().rotation.pitch * pi / 180, vehicle->GetTransform().rotation.roll * pi / 180));
+                 Rotate(vehicle->GetTransform().rotation.yaw * M_PI / 180, vehicle->GetTransform().rotation.pitch * M_PI / 180, vehicle->GetTransform().rotation.roll * M_PI / 180));
     
+    // Draw initial vehicle
     drawCar(poseRef, 0, Color(1, 0, 0), 0.7, viewer);
 
+    // Main viewer loop
     while (!viewer->wasStopped()) {
         while (!new_scan) {
             new_scan = true;
 
-            // Step 1: Filter the point cloud using a voxel grid filter
+            // Step 1: Voxel grid filtering
             pcl::VoxelGrid<PointT> voxelGrid;
             voxelGrid.setInputCloud(scanCloud);
-            voxelGrid.setLeafSize(0.5f, 0.5f, 0.5f); // Set voxel size here
+            voxelGrid.setLeafSize(0.5f, 0.5f, 0.5f); // Adjust voxel size as needed
             voxelGrid.filter(*cloudFiltered);
 
-            // Step 2: Perform pose estimation using ICP or NDT
-            Pose estimatedPose = performICP(mapCloud, cloudFiltered); // or use performNDT for NDT
+            // Step 2: Pose estimation (ICP or NDT)
+            Pose estimatedPose = performICP(mapCloud, cloudFiltered); // or performNDT for NDT
 
-            // Step 3: Transform the scan cloud
+            // Step 3: Transform scan cloud
             pcl::transformPointCloud(*cloudFiltered, *scanCloud, estimatedPose.getTransformationMatrix());
 
+            // Update viewer
             viewer->removeAllShapes();
             drawCar(estimatedPose, 1, Color(0, 1, 0), 0.35, viewer);
             viewer->spinOnce();
         }
 
+        // Refresh viewer if needed
         if (refresh_view) {
             refresh_view = false;
             viewer->removeAllShapes();
@@ -213,4 +228,6 @@ int main() {
 
         viewer->spinOnce();
     }
+
+    return 0;
 }
